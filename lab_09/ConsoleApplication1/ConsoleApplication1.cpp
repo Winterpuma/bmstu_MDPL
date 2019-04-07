30b4cadd54e6ace1c26155deffa698ffcd44cef0
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-04-07 14:53:57 +0300

    Add factorial loop realization

--- lab_09/ConsoleApplication1/ConsoleApplication1.cpp
+++ lab_09/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,11 +3,12 @@
 
 #include "stdafx.h"
 extern "C" void p1(int N, int *g);
+extern "C" void factorial_loop(int N, int *g);
 
 int N = 6, NF = 7;
 int _tmain(int argc, _TCHAR* argv[])
 {
-	p1(N, &NF);
+	factorial_loop(N, &NF);
 	printf("Factorial of %d is %d", N, NF);
 	return 0;
 }

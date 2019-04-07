f93bccea3e693b26450250daf9e79a18cd29e59a
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-04-07 19:26:17 +0300

    Delete _tmain

--- lab_09/ConsoleApplication1/ConsoleApplication1.cpp
+++ lab_09/ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,11 +7,11 @@ extern "C" void factorial_loop(int N, int *g);
 extern "C" void factorial_stack(int N, int *g);
 extern "C" void factorial_registers(int N, int *g);
 
-int N = 6, NF = 1;
+int N = 7, NF = 1;
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
- 	factorial_registers(N, &NF);
+ 	factorial_loop(N, &NF);
 	printf("Factorial of %d is %d", N, NF);
 	return 0;
 }

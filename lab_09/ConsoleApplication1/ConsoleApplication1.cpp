3771b86eb6dca515a1d4e7f30f0e36ccdcc2e464
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-04-07 16:55:55 +0300

    Save non working stack

--- lab_09/ConsoleApplication1/ConsoleApplication1.cpp
+++ lab_09/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,11 +4,22 @@
 #include "stdafx.h"
 extern "C" void p1(int N, int *g);
 extern "C" void factorial_loop(int N, int *g);
+extern "C" void factorial_stack(int N, int *g);
 
-int N = 6, NF = 7;
+int N = 5, NF = 1;
+
+void fact_stack(int N, int *NF)
+{
+	if (N)
+	{
+		*NF *= N;
+		fact_stack(N - 1, NF);
+	}
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	factorial_loop(N, &NF);
+	factorial_stack(N, &NF);
 	printf("Factorial of %d is %d", N, NF);
 	return 0;
 }

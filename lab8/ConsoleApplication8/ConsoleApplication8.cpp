17cefc2bb585781442f4bdcff9551e84b2ff34bc
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-04-09 12:51:24 +0300

    Add partly transponation

--- lab8/ConsoleApplication8/ConsoleApplication8.cpp
+++ lab8/ConsoleApplication8/ConsoleApplication8.cpp
@@ -1,33 +1,34 @@
 #include "stdafx.h"
 #include <conio.h>
 #include <iostream>
-
-#define N 6
+// Puma
+#define N 5 // Size of matr to transpose
+#define B 8 // Size of all matr
 
 using namespace std;
 /*
-unsigned char matr[N][N] = {
-	{1,1,1,1,1},
-	{2,2,2,2,2},
-	{3,3,3,3,3},
-	{4,4,4,4,4},
-	{5,5,5,5,5}
-};*/
-
-unsigned char matr[N][N] = {
+unsigned char matr[B][B] = {
 	{1,1,1,1,1,1},
 	{2,2,2,2,2,2},
 	{3,3,3,3,3,3},
 	{4,4,4,4,4,4},
 	{5,5,5,5,5,5},
 	{6,6,6,6,6,6}
+};*/
+
+unsigned char matr[B][B] = {
+	{1,1,1,1,1},
+	{2,2,2,2,2},
+	{3,3,3,3,3},
+	{4,4,4,4,4},
+	{5,5,5,5,5}
 };
 
 void print_matr()
 {
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < B; i++)
 	{
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < B; j++)
 			printf("%d ", matr[i][j]);
 		printf("\n");
 	}
@@ -43,7 +44,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		mov ECX,N-1
 OuterLoop: 
 		lea ESI,[EBX+1]
-		lea EDI,[EBX+N]
+		lea EDI,[EBX+B]
 
 InnerLoop:
 		XCHG AL,[ESI]
@@ -51,7 +52,7 @@ InnerLoop:
 		XCHG AL,[ESI]
 
 		inc ESI
-		ADD EDI,N
+		ADD EDI,B
 
 		mov EAX,1
 		ADD EAX,ECX
@@ -62,7 +63,7 @@ InnerLoop:
 		jmp CheckOuterLoop
 
 CheckOuterLoop:
-		ADD EBX,N+1
+		ADD EBX,B+1
 		Loop OuterLoop	
 	}
 

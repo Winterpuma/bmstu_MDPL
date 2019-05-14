99930bd4023a55dff8b34cf441c3a2b9a6156413
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-05-14 16:43:06 +0300

    Add pop and push macros

---
+++ lab_12/lab12/lab12.cpp
@@ -0,0 +1,14 @@
+// lab12.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+extern "C" void START();
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    START();
+
+	return 0;
+}
+
